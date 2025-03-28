e564090f6b6585e26874b83e1e658454bdbc93db
AdniSosa <adni.sori@gmail.com>
2025-03-28 14:43:23 +0100

    Terminado

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { Link } from "react-router-dom";
+
+const Home = ({data}) => {
   return (
     <>
-    
+    <h1>Lista de tareas</h1>
+    <ul>
+      {data.map(item => (
+        <li key={item._id}>
+          <Link to={`/${item._id}`}>{item.title}</Link>
+        </li>
+      ))}
+    </ul>
     </>
   )
 };
